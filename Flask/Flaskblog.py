ba3ddc586ac47d249d2cbf68f85eb7209a67ae41
Rakesh Kumar <rkakumar85@gmail.com>
2018-07-27 22:17:00 -0500

    Added templates about and home

--- Flask/Flaskblog.py
+++ Flask/Flaskblog.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Hello World to you all!</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
