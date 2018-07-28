d0058d05a5cc884e4a2ce0bd53c3df3558be1ca2
Rakesh Kumar <rkakumar85@gmail.com>
2018-07-27 22:36:29 -0500

    Added For loop on blogs

--- Flask/Flaskblog.py
+++ Flask/Flaskblog.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+post = [
+    {
+        'author':'Rakesh Kumar A',
+        'title': 'Blog Post 1',
+        'content': 'First Post comment',
+        'date_posted': '27 July, 2018'
+    },
+    {
+        'author': 'Surabhi Bramhadev',
+        'title': 'Blog Post 2',
+        'content': 'Second Post comment',
+        'date_posted': '26 July, 2018'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=post)
 
 @app.route("/about")
 def about():
